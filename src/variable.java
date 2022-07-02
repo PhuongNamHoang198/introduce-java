64247dc4ec7f05f45c7297e04c12672d85ae8c4c
PhuongNamHoang.198 <hoangphuongnam.98@gmail.com>
2022-07-02 11:23:49 +0700

    first commit

---
+++ src/variable.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.function.DoubleToIntFunction;
+
+public class variable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println("i= " +i);
+        System.out.println("f= " +f);
+        System.out.println("d= " +d);
+        System.out.println("b= " +b);
+        System.out.println("c= " +c);
+        System.out.println("s= " +s);
+
+
+
+    }
+}
